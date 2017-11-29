d2aa63b6c9a88a8fff6271c4f88e5420f3359e44
yuval pnueli <yuvalpnueli@gmail.com>
2017-11-29 17:53:39 +0200

    partial nodes

--- HW3/source.hpp
+++ HW3/source.hpp
@@ -7,17 +7,11 @@
 using std::vector;
 using std::string;
 
-struct Table_entry{
-    std::string id;
-    int numline;
-
-};
 
 class Node{
 	public:
 	Node* father;
     vector<Node*> sons;
-	int token_enum;
 	
 	Node(){};
 	
@@ -25,113 +19,58 @@ class Node{
 	void addSon(Node* son)   	{sons.push_back(son);}
 };
 
-/*
-class Program: public Node{
-};
+class ProgramNode: public Node{};
+
+class FuncsNode : public Node{};
+
+class FuncDecl : public Node{};
+
+class RetTypeNode : public Node{};
+
+class FormalsNode : public Node{};
+
+class FormalsListNode : public Node{};
+
+class FormalDeclNode : public Node{};
+
+class StatementsNode : public Node{};
+
+class StatementNode : public Node{};
+
+class CaseListNode : public Node{};
+
+class CaseStatementNode : public Node{};
+
+class CaseDecNode : public Node{};
+
+class CallNode : public Node{};
+
+class ExpListNode : public Node{};
+
+class TypeNode : public Node{};
+
+class ExpNode : public Node{};
+
+class Void : public Node{};
+class Int : public Node{};
+class Byte : public Node{};
+class B_Node: public Node{};
+class Bool : public Node{};
+class And : public Node{};
+class Or: public Node{};
+class Not : public Node{};
+class True : public Node{};
+class False: public Node{};
+class Return : public Node{};
+class If : public Node{};
+class Else : public Node{};
+class While : public Node{};
+class Switch : public Node{};
+class Case : public Node{};
+class Break : public Node{};
+class Default : public Node{};
+class Colon : public Node{};
+class SC_Node : public Node{};
 
 
 
-class Funcs: public Node{
-};
-
-
-class FuncDecl: public Node{
-};
-
-
-class RetType: public Node{
-
-};
-
-
-class Statements: public Node{
-};
-
-
-class Statement: public Node{
-};
-
-
-
-class ID: public Node{
-};
-
-
-class Formals: public Node{
-};
-
-
-
-class Type: public Node{
-    int type;
-};
-
-*/
-
-
-class Num: public Node{
-	public:
-	int value;
-	Num(int val):value(val){};
-	Num(string val):value(stoi(val)){};
-	int eval(void) { return value; }
-};
-
-class Int: public Node{
-	public:
-	Int(int enumVal){token_enum=enumVal;};
-};
-
-class Double: public Node{
-	public:
-	Double(int enumVal){token_enum=enumVal;};
-};
-class Assign: public Node{};
-class Plus: public Node{};
-class Minus: public Node{};
-
-class Type: public Node{
-	public:
-    int type;
-	Type(Double* son_double):type(son_double->token_enum){};
-	Type(Int* son_int):type(son_int->token_enum){};
-};
-
-class ID: public Node{
-	public:
-    string name;
-	ID(string myName):name(myName){};
-};
-
-class Exp: public Node{
-	public:
-	int value;
-	Exp(Num* sonNum):value(sonNum->value){};
-	Exp(Exp* sonLeft,Plus* pls, Exp* sonRight):value(sonLeft->value + sonRight->value){
-		addSon(sonLeft);
-		addSon(pls);
-		addSon(sonRight);
-		};
-	Exp(Exp* sonLeft,Minus* mns, Exp* sonRight):value(sonLeft->value - sonRight->value){
-		addSon(sonLeft);
-		addSon(mns);
-		addSon(sonRight);
-		};
-};
-
-class Line: public Node{
-	public:
-	Line(Node* myExp){
-		std::cout << ((Exp*)myExp)->value << std::endl;
-		addSon(myExp);
-	};
-};
-
-class Prog: public Node{
-	public:	
-	Prog(){};
-	Prog(Node* otherProg, Node* myLine){
-		addSon(otherProg);
-		addSon(myLine);
-	}
-};
