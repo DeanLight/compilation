4989f4eaefe1e9fc2405e8f27d0b59479da7fe57
DeanLight <light.skep@gmail.com>
2018-01-17 16:03:28 +0200

    tester working now

--- HW5/run_test.sh
+++ HW5/run_test.sh
@@ -5,7 +5,7 @@
 
 function get_infile_array {
 	dir=${1}
-	array=`find $dir | grep .in$ | rev | cut -d'.' -f2- | rev | sort  ` # removes the in suffix
+  array=($(find $dir | grep .in$ | rev | cut -d'.' -f2- | rev | sort))  # removes the in suffix
 
 	echo $array
 
@@ -18,14 +18,20 @@ function do_test {
 	tests_folder=$2
 
   #gets the names of all files in tests_folder with suffix .in , removes the '.in'
-  infile_arr=$(get_infile_array $tests_folder)
+  infile_arr=($(get_infile_array $tests_folder))
 
 
 	for i in $infile_arr ; do
     echo checking ${i}.in
-		./${executable} < ${i}.in >! ${i}_mips.s  2>! ${i}.Cerr
-    touch ${i}.myout ${i}.Serr
-    ./spim -file ${i}_mips.s | tee >( grep -E "^@@@" >! ${i}.Serr ) | grep -v -E "^@@@" >! ${i}.myout
+
+    touch ${i}.myout ${i}.Serr ${i}.s ${i}.Cerr
+
+		echo "./${executable} < ${i}.in >! ${i}.s  2>! ${i}.Cerr"
+    ./${executable} < ${i}.in > ${i}.s  2> ${i}.Cerr
+    echo cating ${i}.s
+
+    echo "./spim -file ${i}.s | tee >( grep -E "^@@@" >! ${i}.Serr ) | grep -v -E "^@@@" >! ${i}.myout"
+    ./spim -file ${i}.s | tee >( grep -E "^@@@" > ${i}.Serr ) | grep -v -E "^@@@" > ${i}.myout
 
 		if cmp ${i}.out ${i}.myout
 		then
